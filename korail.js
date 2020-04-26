825fd988f1748efaf9e6653cb88a3a331eeaea1b
psk <psk3060@naver.com>
2020-04-26 21:08:38 +0900

    아코디언 이벤트를 부여하기 위한 정리

--- korail.js
+++ korail.js
@@ -4,30 +4,36 @@
   for( var i = 0; i < subMenu.length; i++ ) {
     (
       function(j) {
-        if(subMenu[j].children[0].addEventListener) {
-          subMenu[j].children[0].addEventListener('click', function() {
-            if(subMenu[j].children[1].style.display === 'block') {
-                subMenu[j].children[1].style.display = "none";
-            } else {
-                subMenu[j].children[1].style.display = "block";
-            }
-            noneGnb(j);
-          }
-        );
+        var sMenu = subMenu[j];
+        if(sMenu.children[0].addEventListener) {
+          sMenu.children[0].addEventListener('click', function() {
+            visibleUL(sMenu, j);
+          });
         } else {
-
+          sMenu.children[0].attachEvent('click', function() {
+            visibleUL(sMenu, j);
+          });
         }
-
       }
     )(i);
   }
 })();
 
+function visibleUL(sMenu, j) {
+  // 비활성화되어 있으면 비활성화 제거 아니면 추가
+  sMenu.children[1].classList.toggle('accordion_unactive');
+  // 활성화되어 있으면 활성화 제거 아니면 추가
+  sMenu.children[1].classList.toggle('accordion_active');
+
+  noneGnb(j);
+}
+
 function noneGnb(k) {
   var subMenu = getGnbMenu();
   for( var i = 0; i < subMenu.length; i++ ) {
     if( i != k ) {
-      subMenu[i].children[1].style.display = "none";
+      subMenu[i].children[1].classList.remove('accordion_active');
+      subMenu[i].children[1].classList.add('accordion_unactive');
     }
   }
 }
@@ -40,7 +46,7 @@ function getGnbMenu() {
   var limitLength = li.length;
 
   for(var i = 0; i < limitLength; i++ ) {
-    if( li[i].className === 'gnb_menu' ) {
+    if( li[i].classList.contains('gnb_menu') ) {
       subMenu.push(li[i]);
     }
   }
