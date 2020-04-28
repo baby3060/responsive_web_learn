07d1ccd41a904887a10d185f9c409480ec7c7c61
psk <psk3060@naver.com>
2020-04-28 13:52:30 +0900

    Slide 메뉴

--- korail.js
+++ korail.js
@@ -1,4 +1,59 @@
 (function() {
+  function slideDuration() {
+    setInterval(setSlide, 6000);
+  }
+
+  slideDuration();
+})();
+
+
+
+function setSlide() {
+  var oldSelIdx = document.hForm.selIdx.value;
+  var selIdx = document.hForm.selIdx.value;
+  if( selIdx > 3 ) {
+    selIdx = 1;
+  } else {
+    selIdx++;
+  }
+  slideChange(oldSelIdx, selIdx);
+}
+
+function slideChange(oldSelIdx, selIdx) {
+  
+  // fadein 효과
+  function fadeIn(el, time) {
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+      last = +new Date();
+
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
+
+    tick();
+  }
+
+  var slideArr = document.getElementsByName('slide');
+
+  var oldSlide = slideArr[oldSelIdx - 1];
+  var newSlide = slideArr[selIdx - 1];
+
+  oldSlide.style.display = 'none';
+  oldSlide.style.opacity = '0';
+  oldSlide.style.left = '100%';
+  oldSlide.style.zIndex = '10';
+
+  newSlide.style.display = 'block';
+  newSlide.style.left = '0';
+  newSlide.style.zIndex = '9';
+  fadeIn(newSlide, 500);
+  document.hForm.selIdx.value = selIdx;
+}
+
+(function() {
   var subMenu = getGnbMenu();
 
   for( var i = 0; i < subMenu.length; i++ ) {
